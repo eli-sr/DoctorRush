975fe1e476e88b5c7b3e0d6270953aec3df37d98
eli <elzr.sr@gmail.com>
2023-03-13 16:08:47 +0100

    Checkpoint -> Reorganizado script js

--- checkpoint/doctor_rush.js
+++ checkpoint/doctor_rush.js
@@ -1,3 +1,5 @@
+// *** CONSTANTES *** //
+
 // Game container
 const game = document.getElementById('game-container')
 const styleGame = getComputedStyle(game)
@@ -8,14 +10,37 @@ const heightGame = parseInt(styleGame.height)
 const doctor = document.getElementById('doctor')
 const styleDoctor = getComputedStyle(doctor)
 const widthDoctor = parseInt(styleDoctor.width)
-var posDoctor = parseInt(styleDoctor.left)
 const stepDoctor = 20
 
-function moveIsOk(destination) {
-  return true
-  // return destination >= 0 && destination <= widthGame - widthDoctor
-}
+// Zombies
+const widthZombie = widthDoctor
+const zombies = game.getElementsByClassName('zombie')
 
+// Bullets
+const shootArea = document.getElementById('shot-area')
+
+
+// *** VARIABLES GLOBALES *** //
+
+// Logica
+var score = 0
+let gameOver = false
+
+// Doctor
+var posDoctor = parseInt(styleDoctor.left)
+
+// Zombies
+let bufferAddLimit = 1500
+let bufferAdd = 0
+let bufferMoveLimit = 1000
+let bufferMove = 0
+let addZombies
+let moveZombies
+
+
+// *** FUNCIONES *** //
+
+// Logica
 function move(element, dir, step) {
   var pos = parseInt(getComputedStyle(element).top)
   if (dir === 'left' || dir === 'right') pos = parseInt(getComputedStyle(element).left)
@@ -26,6 +51,49 @@ function move(element, dir, step) {
   else element.style.top = `${dest}px`
 }
 
+function vector(xM, yM, xD, yD, step) {
+  const x = xM - xD
+  const y = yM - yD
+  const orModule = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
+  const xFinal = Math.floor((x / orModule) * step)
+  const yFinal = Math.floor((y / orModule) * step)
+  return [xFinal, yFinal]
+}
+
+function checkGameOver(zombieArray) {
+  for (let i = 0; i < zombieArray.length; i++) {
+    const zombie = zombies[i]
+    if (checkCollision(doctor, zombie)) {
+      clearInterval(moveZombies)
+      clearInterval(addZombies)
+      console.log('[!] Game over!')
+      gameOver = true
+    }
+  }
+}
+
+function isOut(element) {
+  const style = getComputedStyle(element)
+  const width = parseInt(style.width)
+  const height = parseInt(style.height)
+  const top = parseInt(element.style.top)
+  const left = parseInt(element.style.left)
+  return top + height < 0 || top > heightGame || left + width < 0 || left > widthGame
+}
+
+function checkCollision(element1, element2) {
+  const rect1 = element1.getBoundingClientRect()
+  const rect2 = element2.getBoundingClientRect()
+  return rect1.right > rect2.left && rect1.left < rect2.right && rect1.bottom > rect2.top && rect1.top < rect2.bottom
+}
+
+function updateScore(inc) {
+  const scorehtml = document.getElementById('score')
+  score += inc
+  scorehtml.innerHTML = score
+}
+
+// Doctor
 function moveDoctor(event) {
   if (gameOver) return
   const key = event.key
@@ -43,19 +111,14 @@ function moveDoctor(event) {
   }
 }
 
-// Zombies
-
-const widthZombie = widthDoctor
-
-function randomPos() {
+// Zombie
+function randomZombiePos() {
   const min = 0
   const max = widthGame - widthZombie
   const randomNum = Math.floor(Math.random() * (max - min + 1)) + min
   return randomNum
 }
 
-let bufferAddLimit = 1500
-let bufferAdd = 0
 function addZombie() {
   const limit = bufferAddLimit - Math.sqrt((bufferAddLimit * score) / 2)
   if (bufferAdd < limit) {
@@ -66,16 +129,11 @@ function addZombie() {
   // console.log('add', bufferAddLimit - score, ':', limit)
   const zombie = document.createElement('div')
   zombie.classList.add('zombie')
-  zombie.style.left = `${randomPos()}px`
+  zombie.style.left = `${randomZombiePos()}px`
   zombie.setAttribute('data-hp', 3)
   game.appendChild(zombie)
 }
 
-var score = 0
-const zombies = game.getElementsByClassName('zombie')
-
-let bufferMoveLimit = 1000
-let bufferMove = 0
 function moveAllZombies() {
   const limit = bufferMoveLimit - Math.sqrt((bufferMoveLimit * score) / 2)
   if (bufferMove < limit) {
@@ -95,32 +153,7 @@ function moveAllZombies() {
   checkGameOver(zombies)
 }
 
-// Game Over
-
-let gameOver = false
-function checkGameOver(zombieArray) {
-  for (let i = 0; i < zombieArray.length; i++) {
-    const zombie = zombies[i]
-    if (checkCollision(doctor, zombie)) {
-      clearInterval(moveZombies)
-      clearInterval(addZombies)
-      console.log('[!] Game over!')
-      gameOver = true
-    }
-  }
-}
-
 // Bullet
-
-function vector(xM, yM, xD, yD, step) {
-  const x = xM - xD
-  const y = yM - yD
-  const orModule = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
-  const xFinal = Math.floor((x / orModule) * step)
-  const yFinal = Math.floor((y / orModule) * step)
-  return [xFinal, yFinal]
-}
-
 function addBullet() {
   const bullet = document.createElement('div')
   bullet.classList.add('bullet')
@@ -128,15 +161,6 @@ function addBullet() {
   bullet.style.top = `${parseInt(styleDoctor.top)}px`
   game.appendChild(bullet)
   return bullet
-}
-
-function isOut(element) {
-  const style = getComputedStyle(element)
-  const width = parseInt(style.width)
-  const height = parseInt(style.height)
-  const top = parseInt(element.style.top)
-  const left = parseInt(element.style.left)
-  return top + height < 0 || top > heightGame || left + width < 0 || left > widthGame
 }
 
 function shoot(event) {
@@ -159,26 +183,6 @@ function shoot(event) {
   var moveBullet = setInterval(moveBulletXY, 40)
 }
 
-const shootArea = document.getElementById('shot-area')
-let addZombies
-let moveZombies
-
-function initializeGame() {
-  document.addEventListener('keypress', moveDoctor)
-  shootArea.addEventListener('click', shoot)
-  addZombies = setInterval(addZombie, 20)
-  moveZombies = setInterval(moveAllZombies, 10)
-}
-
-// Score
-function updateScore(inc) {
-  const scorehtml = document.getElementById('score')
-  score += inc
-  scorehtml.innerHTML = score
-}
-
-// Collision
-
 function isCollidingBullet(bullet) {
   const zombies = game.getElementsByClassName('zombie')
   for (let i = 0; i < zombies.length; i++) {
@@ -195,10 +199,17 @@ function isCollidingBullet(bullet) {
   }
 }
 
-function checkCollision(element1, element2) {
-  const rect1 = element1.getBoundingClientRect()
-  const rect2 = element2.getBoundingClientRect()
-  return rect1.right > rect2.left && rect1.left < rect2.right && rect1.bottom > rect2.top && rect1.top < rect2.bottom
+// MAIN
+function moveIsOk(destination) {
+  return true
+  // return destination >= 0 && destination <= widthGame - widthDoctor
+}
+
+function initializeGame() {
+  document.addEventListener('keypress', moveDoctor)
+  shootArea.addEventListener('click', shoot)
+  addZombies = setInterval(addZombie, 20)
+  moveZombies = setInterval(moveAllZombies, 10)
 }
 
 window.onload = function () {
